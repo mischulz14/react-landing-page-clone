4ecebe5638a502e807baf3c58053804bfd6f5c4f
Michael Schulz <mi.schulz@hotmail.com>
2022-09-26 13:19:18 +0200

    Add navbar animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,26 @@
 import './Navbar.css';
+import { useEffect, useState } from 'react';
 
 export default function Navbar() {
+  const [change, setChange] = useState(false);
+
+  function controlNavbar() {
+    if (window.scrollY > 0) {
+      setChange(true);
+    } else {
+      setChange(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+    return () => {
+      window.removeEventListener('scroll', controlNavbar);
+    };
+  }, []);
+
   return (
-    <nav className="nav">
+    <nav className={`nav ${change ? 'active' : ''}`}>
       <ul className="nav__list">
         <li>Jira Apps</li>
         <li>About Us</li>
